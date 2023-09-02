d18485b5af13e4844054c1c3dd03f6d7ae69f3b9
cidmodder <acc4489@aol.com>
2023-09-01 22:01:22 -0400

    added style and fonts

--- Documents/Codecademy/Mixed_Messages_Project/Mixed_Messages_Java.js
+++ Documents/Codecademy/Mixed_Messages_Project/Mixed_Messages_Java.js
@@ -1,6 +1,6 @@
-let first = ['you enter a tavern', 'you enter a dungeon', 'you enter a castle'];
-let second = ['you see an old friend off to one side', 'you smell something foul coming from nearby', 'you see someone doing their best to sneak by'];
-let third = ['do you speak to your friend or continue on?', 'do you find the source of the smell or get as far away from it as you can?', 'do you follow them to see what they plan or do you ignore them?'];
+let first = ['You enter a tavern', 'You enter a dungeon', 'You enter a castle'];
+let second = ['You see an old friend off to one side', 'You smell something foul coming from nearby', 'You see someone doing their best to sneak by'];
+let third = ['Do you speak to your friend or continue on?', 'Do you find the source of the smell or get as far away from it as you can?', 'Do you follow them to see what they plan or do you ignore them?'];
 let button = document.getElementById('generate');
 
 let num1 = Math.floor(Math.random()*first.length);
